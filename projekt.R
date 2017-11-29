67209e621172c0d7b1cc5998104ccc6352fea779
Karl <kandzio271@gmail.com>
2017-11-30 00:43:20 +0100

    Implements decision tree building
    Implements classification tree building from rpart package
    and displays trees views and parameters.
    Tests trees with verification data sets.

--- projekt.R
+++ projekt.R
@@ -21,9 +21,40 @@ load.data <- function(file_name) {
 }
 redWine <- load.data("winequality/winequality-red.csv")
 whiteWine <- load.data("winequality/winequality-white.csv")
+#spliting redWine into training and verifing data sets
 v <- split(redWine, 1:2)
 redWineT <- v$`1`
 redWineV <- v$`2`
+#spliting whiteWine into training and verifing data sets
 v <- split(whiteWine, 1:2)
 whiteWineT <- v$`1`
 whiteWineV <- v$`2`
+#generating trees with rpart
+redFit <- rpart(quality ~ chlorides+density+pH+sulphates+alcohol+
+                 fixedAcidity+volatileAcidity+citricAcid+
+                 residualSugar+freeSulfurDioxide,
+             data = redWineT,
+             method = "class")
+whiteFit <- rpart(quality ~ chlorides+density+pH+sulphates+alcohol+
+                  fixedAcidity+volatileAcidity+citricAcid+
+                  residualSugar+freeSulfurDioxide,
+                data = whiteWineT,
+                method = "class")
+printcp(redFit) # display the results
+plotcp(redFit) # visualize cross-validation results
+summary(redFit) # detailed summary of splits
+# plot tree
+plot(redFit, uniform=TRUE,
+     main="Classification Tree for red wine quality")
+text(redFit, use.n=TRUE, all=TRUE, cex=.8)
+
+printcp(whiteFit) # display the results
+plotcp(whiteFit) # visualize cross-validation results
+summary(whiteFit) # detailed summary of splits
+# plot tree
+plot(whiteFit, uniform=TRUE,
+     main="Classification Tree for white wine quality")
+text(whiteFit, use.n=TRUE, all=TRUE, cex=.8)
+#predicting for verifing data sets
+print(table(predict(redFit, redWineV, type = "class")))
+print(table(predict(whiteFit, whiteWineV, type = "class")))
